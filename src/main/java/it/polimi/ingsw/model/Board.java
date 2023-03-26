32d628782980fd5ca69a97ff21fd8bfbce338dfe
GiuliaRossi2 <126389691+GiuliaRossi2@users.noreply.github.com>
2023-03-26 17:23:54 +0200

    change board

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -2,214 +2,114 @@ package it.polimi.ingsw.model;
 
 
 
+
 import java.util.ArrayList;
-import java.util.Random;
-public class Board {
+import java.util.Observable;
+
+public class Board extends Observable {
     private BoardBox[][] matrix;
-    private ArrayList<ItemTile> tiles;
-    private ArrayList<BoardBox> selectedBoard;
 
-    public void fill(int [][] matrice){
-        //importa da jason la matrice
-        int dimensione = 9;   // o qualsiasi altra dimensione desiderata
-        this.matrix = new BoardBox[dimensione][dimensione];
-        Random random=new Random();
-        int randomNumber;
-        for (int i = 0; i < matrice.length; i++) {
-            for (int j = 0; j < matrice[i].length; j++) {
-                this.matrix[i][j] = new BoardBox(i,j);
-                if(matrice[i][j]==1){
-                    randomNumber = random.nextInt(tiles.size());
-                    matrix[i][j].setTile(tiles.get(randomNumber));
-                    tiles.remove(randomNumber);
-                    matrix[i][j].setoccupiable(true);
-                    matrix[i][j].setoccupied(true);
-                }
-            }
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j].getoccupied()){
-                    setFreeEdges(i,j);
-
-                }
-            }
-        }
-    }
+    private boolean finishPlayer;
 
 
-    public void initialize() {
-        //IMPORTARE IL 22
-        //IMPORTARE IL 22 DA un FILE JASON
-        int j=0;
-        tiles=new ArrayList<ItemTile>();
-        for(Type t:Type.values()){
-            for (int i = 0; i < 22; i++) {
-                tiles.add(new ItemTile(t,j++));
-            }
-        }
-
-    }
-
-    public void setFreeEdges(int x, int y){
-        if(x>0) {
-            //sopra
-            if (!matrix[x - 1][y].getoccupied() || !matrix[x - 1][y].getoccupiable())
-                matrix[x][y].increasefreeEdges();
-        }else matrix[x][y].increasefreeEdges();
-        if(x<matrix.length-1) {
-            //sotto
-            if (!matrix[x + 1][y].getoccupied()|| !matrix[x + 1][y].getoccupiable())
-                matrix[x][y].increasefreeEdges();
-        }else matrix[x][y].increasefreeEdges();
-        if(y>0) {
-            //sinistra
-            if (!matrix[x][y - 1].getoccupied()|| !matrix[x][y-1].getoccupiable())
-                matrix[x][y].increasefreeEdges();
-        }else matrix[x][y].increasefreeEdges();
-        if(y<matrix.length-1) {
-            //destra
-            if (!matrix[x][y + 1].getoccupied()|| !matrix[x][y+1].getoccupiable())
-                matrix[x][y].increasefreeEdges();
-        }else matrix[x][y].increasefreeEdges();
-    }
-
-    public void increaseNear(int x, int y){
-        if(x>0) {
-            //sopra
-            if (matrix[x-1][y].getoccupied())
-                matrix[x-1][y].increasefreeEdges();
-        }
-        if(x<matrix.length-1) {
-            //sotto
-            if (matrix[x+1][y].getoccupied())
-                matrix[x+1][y].increasefreeEdges();
-        }
-        if(y>0) {
-            //sinistra
-            if (matrix[x][y-1].getoccupied())
-                matrix[x][y-1].increasefreeEdges();
-        }
-        if(y< matrix.length-1) {
-            //destra
-            if (matrix[x][y+1].getoccupied())
-                matrix[x][y+1].increasefreeEdges();
-        }
-    }
-    private boolean Near(BoardBox boardbox,int nTessera) {
-        if
-        ((selectedBoard.get(nTessera - 1).getX() - boardbox.getX() == 1 ||
-                selectedBoard.get(nTessera - 1).getX() - boardbox.getX() == -1 ||
-                selectedBoard.get(nTessera - 1).getY() - boardbox.getY() == 1 ||
-                selectedBoard.get(nTessera - 1).getY() - boardbox.getY() == -1)
-                && (selectedBoard.get(nTessera - 1).getX() == boardbox.getX() ||
-                selectedBoard.get(nTessera - 1).getY() == boardbox.getY())) {
-            return true;
-        }
-        return false;
-    }
-    private boolean SamerowOrSamecolumn(BoardBox boardbox,int nTessera) {
-        if(
-                (selectedBoard.get(nTessera-1).getX()==boardbox.getX() &&
-                        selectedBoard.get(nTessera-2).getX()==boardbox.getX()) ||
-                        (selectedBoard.get(nTessera-1).getY()==boardbox.getY() &&
-                                selectedBoard.get(nTessera-2).getY()==boardbox.getY())) {
-            return true;
-        }
-        return false;
-    }
-    public boolean isSelectable(BoardBox boardbox,int nTessera){
-        switch(nTessera) {
-            case 0:
-                if(boardbox.getEdges()>0){
-                    selectedBoard= new ArrayList<BoardBox>();
-                    selectedBoard.add(boardbox);
-                    return true;
-                }
-                break;
-            case 1:
-                if(boardbox.getEdges()>0 && Near(boardbox,nTessera)){
-                    selectedBoard.add(boardbox);
-                    return true;
-                }
-                break;
-            case 2:
-                if(boardbox.getEdges()>0 && Near(boardbox,nTessera)&& SamerowOrSamecolumn(boardbox,nTessera)){
-                    selectedBoard.add(boardbox);
-                    return true;
-                }
-                break;
-            default:
-
-                break;
-        }
-        return false;
-    }
-    public ArrayList<ItemTile> Selected(){
-        ArrayList<ItemTile>  selectedItems= new ArrayList<ItemTile>();
-        for(int i=0;i<selectedBoard.size();i++ ){
-            selectedItems.add(selectedBoard.get(i).getItemtile());
-            matrix[selectedBoard.get(i).getX()][selectedBoard.get(i).getY()].setoccupied(false);
-            increaseNear(selectedBoard.get(i).getX(),selectedBoard.get(i).getY());
-            matrix[selectedBoard.get(i).getX()][selectedBoard.get(i).getY()].setTile(null);
-            matrix[selectedBoard.get(i).getX()][selectedBoard.get(i).getY()].setEdges(0);
-        }
-        for(int i=0;i<selectedItems.size();i++){
-            System.out.println(selectedItems.get(i).getType());
-        }
-        return selectedItems;
-    }
-
-
-    public boolean checkRefill(){
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j].getoccupied()){
-                    if(matrix[i][j].getEdges()!=4)
-                        return false;
-                }
-
-            }
-        }
-        return true;
-
-    }
-    public void refill(){
-        //sposta le tessere isolate nel sacchetto
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j].getoccupied()){
-                    tiles.add(matrix[i][j].getItemtile());
-                    matrix[i][j].setItemtile(null);
-                    matrix[i][j].setoccupied(false);
-                }
-            }
-        }
-        Random random=new Random();
-        int randomNumber;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j].getoccupiable()){
-                    randomNumber = random.nextInt(tiles.size());
-                    matrix[i][j].setTile(tiles.get(randomNumber));
-                    tiles.remove(randomNumber);
-                    matrix[i][j].setoccupied(true);
-                }
-            }
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j].getoccupied()){
-                    matrix[i][j].setEdges(0);
-                    setFreeEdges(i,j);
-                }
-            }
-        }
-    }
-
-    public BoardBox getBoardBox(int x,int y){
+    public BoardBox getBoardBox(int x,int y) {
         return matrix[x][y];
     }
 
+    public BoardBox[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(BoardBox[][] matrix) {
+        this.matrix = matrix;
+        setChanged();
+        notifyObservers(matrix);
+    }
+
+    private ArrayList<ItemTile> tiles;
+
+    public ArrayList<ItemTile> getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(ArrayList<ItemTile> tiles) {
+        this.tiles = tiles;
+        setChanged();
+        notifyObservers(tiles);
+    }
+
+    private ArrayList<BoardBox> selectedBoard;
+
+    public ArrayList<BoardBox> getSelectedBoard() {
+        return selectedBoard;
+    }
+
+    public void setSelectedBoard(ArrayList<BoardBox> selectedBoard) {
+
+        this.selectedBoard = selectedBoard;
+        setChanged();
+        notifyObservers(selectedBoard);
+    }
+
+    private BoardBox[][] selectable;
+
+    private Integer playerChoiceX;
+
+    public Integer getPlayerChoiceX() {
+        return playerChoiceX;
+    }
+
+    public void setPlayerChoiceX(Integer playerChoiceX) {
+        this.playerChoiceX = playerChoiceX;
+    }
+
+
+    private Integer playerChoiceY;
+
+
+
+
+    public Integer getPlayerChoiceY() {
+        return playerChoiceY;
+    }
+
+    public void setPlayerChoiceY(Integer playerChoiceY) {
+        this.playerChoiceY = playerChoiceY;
+    }
+
+
+
+    public Board() {
+        playerChoicenumTile=-1;
+        playerChoiceX=-1;
+        playerChoiceY=-1;
+    }
+
+
+    public boolean isFinishPlayer() {
+        return finishPlayer;
+    }
+
+    public void setFinishPlayer(boolean finishPlayer) {
+        this.finishPlayer = finishPlayer;
+        setChanged();
+        notifyObservers(finishPlayer);
+    }
+    public void setFinishPlayeropposite() {
+        finishPlayer = finishPlayer;
+        setChanged();
+        notifyObservers(finishPlayer);
+    }
+
+
+    private Integer playerChoicenumTile;
+    public Integer getPlayerChoicenumTile() {
+        return playerChoicenumTile;
+    }
+
+    public void setPlayerChoicenumTile(Integer playerChoicenumTile) {
+        this.playerChoicenumTile = playerChoicenumTile;
+    }
 }
+
 
