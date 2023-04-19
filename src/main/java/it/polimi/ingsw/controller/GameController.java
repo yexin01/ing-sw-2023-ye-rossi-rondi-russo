1865a4036c4f3b3068c823203a2035c323edf656
GiuliaRossi2 <126389691+GiuliaRossi2@users.noreply.github.com>
2023-04-19 15:39:01 +0200

    delete player controller and change method

--- src/main/java/it/polimi/ingsw/controller/GameController.java
+++ src/main/java/it/polimi/ingsw/controller/GameController.java
@@ -5,11 +5,13 @@ import it.polimi.ingsw.exceptions.ErrorType;
 import it.polimi.ingsw.json.GameRules;
 
 import it.polimi.ingsw.messages.MessageFromClient;
+import it.polimi.ingsw.messages.MessagePayload;
+import it.polimi.ingsw.model.BoardBox;
 import it.polimi.ingsw.model.Game;
 
 
 public class GameController implements Controller{
-    private PlayerController playerController;
+
     /*
         private SetupController setupController;
 
@@ -23,18 +25,16 @@ public class GameController implements Controller{
         this.game=game;
         initializeControllers();
     }
-    public Game getModel() {
-        return game;
-    }
 
     public TurnController getTurnController() {
         return turnController;
     }
-
+    public Game getModel() {
+        return game;
+    }
     public void initializeControllers() throws Exception {
         GameRules gameRules=new GameRules();
         int maxPlayers=gameRules.getMaxPlayers();
-        playerController=new PlayerController(this);
         turnController=new TurnController(game);
         turnController.setCurrentPhase(TurnPhase.SELECT_FROM_BOARD);
     }
@@ -56,13 +56,115 @@ public class GameController implements Controller{
             if (!nicknamePlayer.equals(turnController.getTurnPlayer().getNickname())) {
                 throw new Error(ErrorType.ILLEGAL_TURN);
             }
-            playerController.turnPhase(message);
+            turnPhase(message);
 
         }catch(Exception e){
 
         }
 
 
+    }
+    public void turnPhase(MessageFromClient message) throws Exception {
+        String messageName = message.getClientMessageHeader().getMessageName();
+        MessagePayload payload = message.getMessagePayload();
+        if (!TurnPhase.contains(messageName)) throw new IllegalArgumentException();
+        //try {
+        switch (messageName) {
+            case "SelectionBoard" -> checkAndInsertBoardBox(message);
+            case "FinishSelectionBoard" -> associatePlayerTiles();
+            //  case "RESET_PLAYER_CHOICE_TILES" -> ;
+            case "SelectOrder" -> permutePlayerTiles(message);
+            case "SelectColumn" -> selectingColumn(message);
+            case "InsertBookshelfAndPoint" -> insertBookshelf();
+            default -> throw new IllegalArgumentException();
+        }
+        //} catch (Error e) {
+        //}
+    }
+
+
+    public void checkAndInsertBoardBox( MessageFromClient message) throws Error {
+        illegalPhase(TurnPhase.SELECT_FROM_BOARD);
+
+        int x=message.getMessagePayload().getX();
+        int y=message.getMessagePayload().getY();
+        game.getBoard().checkCoordinates(x,y);
+
+        System.out.println("You selected "+message.getMessagePayload().getX()+","+message.getMessagePayload().getY());
+        BoardBox boardBox=game.getBoard().getBoardBox(message.getMessagePayload().getX(),message.getMessagePayload().getY());
+        int maxPlayerSelectableTiles=game.getTurnPlayer().getBookshelf().numSelectableTiles();
+        game.getBoard().checkSelectable(boardBox,maxPlayerSelectableTiles);
+
+    }
+
+
+    public void associatePlayerTiles() throws Error {
+        illegalPhase(TurnPhase.SELECT_FROM_BOARD);
+        game.getTurnPlayer().selection(game.getBoard());
+        turnController.changePhase();
+        turnController.changePhase();
+        //TODO poi andrà tolta
+        // gameController.getTurnController().changePhase();
+    }
+    public void permutePlayerTiles(MessageFromClient message) throws Error {
+        illegalPhase(TurnPhase.SELECT_ORDER_TILES);
+        int[] orderTiles=message.getMessagePayload().getOrderTiles();
+        //TODO check Order
+        if (!true){
+            throw new Error(ErrorType.INVALID_ORDER_TILE);
+        }
+        game.getTurnPlayer().permuteSelection(orderTiles);
+        turnController.changePhase();
+    }
+
+
+    public void selectingColumn(MessageFromClient message) throws Error {
+        illegalPhase(TurnPhase.SELECT_COLUMN);
+        int column=message.getMessagePayload().getColumn();
+        System.out.println("You selected "+column);
+        game.getTurnPlayer().getBookshelf().checkBookshelf(column,game.getTurnPlayer().getSelectedItems().size());
+        /*
+        if(!(gameController.getModel().getTurnPlayer().getBookshelf().checkBookshelf(column,gameController.getModel().getTurnPlayer().getSelectedItems().size()))){
+            //TODO decide how to handle the exception
+            System.err.println("Invalid column");
+            fireError(ErrorMessageType.INVALID_COLUMN, "Invalid column");
+            throw new NotEnoughFreeCellsColumn();
+        }
+
+         */
+        game.getTurnPlayer().getBookshelf().setColumnSelected(column);
+        turnController.changePhase();
+    }
+
+
+    public void insertBookshelf() throws Exception {
+        illegalPhase(TurnPhase.INSERT_BOOKSHELF_AND_POINTS);
+        game.getTurnPlayer().insertBookshelf();
+        if(game.getTurnPlayer().getBookshelf().isFull()){
+            game.setEndGame(true);
+        }
+        game.updateAllPoints();
+        finishTurn();
+
+    }
+    public void finishTurn() {
+        if(game.getBoard().checkRefill()){
+            game.getBoard().refill();
+        }
+        //TODO da finire
+        if(game.isEndGame() && game.getTurnPlayer().equals(game.getLastPlayer())){
+            //listeners.firePropertyChange(new PropertyChangeEvent(gameController.getModel().getTurnPlayer(), "EndGame",
+            //       null,null));
+        }
+        game.setNextPlayer();
+        turnController.setCurrentPhase(TurnPhase.SELECT_FROM_BOARD);
+    }
+
+    public void illegalPhase(TurnPhase phase) throws Error {
+        if(!turnController.getCurrentPhase().equals(phase)){
+            throw new Error(ErrorType.ILLEGAL_PHASE);
+        }
+        return;
     }
 
 }
