faa6f37d897fb876f95582e632212244f2dd10f6
andrearondi <126339408+andrearondi@users.noreply.github.com>
2023-04-20 12:29:31 +0200

    changes in rmihandler and implementation
    Signed-off-by: xin <xin.ye@mail.polimi.it>

--- src/main/java/it/polimi/ingsw/network/server/RMIHandler.java
+++ src/main/java/it/polimi/ingsw/network/server/RMIHandler.java
@@ -1,4 +1,24 @@
 package it.polimi.ingsw.network.server;
 
-public class RMIHandler {
+import it.polimi.ingsw.network.client.RMIClientConnection;
+import it.polimi.ingsw.network.messages.Message;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * This interface is the one sent to the RMI client enabling him to send messages to server
+ */
+public interface RMIHandler extends Remote {
+
+    void login(String username, RMIClientConnection client) throws RemoteException;
+
+    /**
+     * Sends a message to the server
+     *
+     * @param message message sent to server
+     */
+    void onMessage(Message message) throws RemoteException;
+
+    void disconnectMe() throws RemoteException;
 }
--- src/main/java/it/polimi/ingsw/network/server/RMIHandlerImplementation.java
+++ src/main/java/it/polimi/ingsw/network/server/RMIHandlerImplementation.java
@@ -1,4 +1,64 @@
 package it.polimi.ingsw.network.server;
 
-public class RMIHandlerImplementation {
+import it.polimi.ingsw.network.client.RMIClientConnection;
+import it.polimi.ingsw.network.messages.Message;
+
+import java.io.Serial;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Objects;
+
+/**
+ * This class is the implementation of the interface RMIHandler
+ */
+public class RMIHandlerImplementation extends UnicastRemoteObject implements RMIHandler {
+    @Serial
+    private static final long serialVersionUID = 3776047796084380457L;
+
+    private final transient Server server;
+    private transient RMIConnection rmiSession;
+
+    RMIHandlerImplementation(Server server) throws RemoteException {
+        this.server = server;
+    }
+
+
+    @Override
+    public void login(String username, RMIClientConnection client) {
+        rmiSession = new RMIConnection(server, client);
+        server.login(username, rmiSession);
+    }
+
+    /**
+     * Sends a message to the server
+     *
+     * @param message message sent to server
+     */
+    @Override
+    public void onMessage(Message message) {
+        server.onMessage(message);
+    }
+
+    @Override
+    public void disconnectMe() {
+        rmiSession.disconnect();
+    }
+
+/*
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        RMIHandlerImplementation that = (RMIHandlerImplementation) o;
+        return Objects.equals(server, that.server) &&
+                Objects.equals(rmiSession, that.rmiSession);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), server, rmiSession);
+    }
+
+ */
 }
