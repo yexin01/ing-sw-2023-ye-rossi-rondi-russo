5fe8539de4803a6789d3f811bec3a04a2c29b726
Samuele Russo <samuele.russo@mail.polimi.it>
2023-04-22 08:35:53 +0200

    Remaining Common goal cards tests added after previous merge error

---
+++ src/test/java/it/polimi/ingsw/model/CommonGoalCard8Test.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommonGoalCard8Test {
+
+    @Test
+    @DisplayName("Generic true check for CommonGoal8")
+    void checkGoal() {
+        CommonGoalCard8 commonGoalCard8 = new CommonGoalCard8();
+        Bookshelf bookshelf = new Bookshelf(6,5,3);
+        int tileID = 0;
+        bookshelf.getMatrix()[0][0] = new ItemTile(Type.CAT, tileID); tileID++;
+        bookshelf.getMatrix()[0][4] = new ItemTile(Type.CAT, tileID); tileID++;
+        bookshelf.getMatrix()[5][0] = new ItemTile(Type.CAT, tileID); tileID++;
+        bookshelf.getMatrix()[5][4] = new ItemTile(Type.CAT, tileID);
+        assertTrue(commonGoalCard8.checkGoal(bookshelf.getMatrix()));
+    }
+
+    @Test
+    @DisplayName("Corner case: one different tile type")
+    void checkGoalCC1() {
+        CommonGoalCard8 commonGoalCard8 = new CommonGoalCard8();
+        Bookshelf bookshelf = new Bookshelf(6,5,3);
+        int tileID = 0;
+        bookshelf.getMatrix()[0][0] = new ItemTile(Type.CAT, tileID); tileID++;
+        bookshelf.getMatrix()[0][4] = new ItemTile(Type.CAT, tileID); tileID++;
+        bookshelf.getMatrix()[5][0] = new ItemTile(Type.CAT, tileID); tileID++;
+        bookshelf.getMatrix()[5][4] = new ItemTile(Type.TROPHY, tileID);
+        assertFalse(commonGoalCard8.checkGoal(bookshelf.getMatrix()));
+    }
+}
